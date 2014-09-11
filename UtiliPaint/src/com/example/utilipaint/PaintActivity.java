05c5ff09e38e13b06f4a005936920e15ca63197a
988305 <988305@152-ZLB14-323.mcpsmd.org>
2014-09-11 14:10:27 -0400

    Add Single Context Menu

--- UtiliPaint/src/com/example/utilipaint/PaintActivity.java
+++ UtiliPaint/src/com/example/utilipaint/PaintActivity.java
@@ -2,21 +2,41 @@ package com.example.utilipaint;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class PaintActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_paint);
+
+		this.registerForContextMenu(this.findViewById(R.id.action_file));
+
+		this.setContentView(R.layout.activity_paint);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.paint, menu);
+		this.getMenuInflater().inflate(R.menu.paint, menu);
 		return true;
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = this.getMenuInflater();
+
+		switch (v.getId()) {
+		case R.id.action_file:
+			inflater.inflate(R.menu.file_context_menu, menu);
+			break;
+		}
 	}
 
 }
