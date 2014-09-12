f3cedcb784e08c95f64645c31a47634d186095c2
Jonathan Ni <***@gmail.com>
2014-09-12 12:50:20 -0400

    Add Popup Menu for File

--- UtiliPaint/src/com/example/utilipaint/PaintActivity.java
+++ UtiliPaint/src/com/example/utilipaint/PaintActivity.java
@@ -6,15 +6,15 @@ import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 
 public class PaintActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		this.registerForContextMenu(this.findViewById(R.id.action_file));
 
 		this.setContentView(R.layout.activity_paint);
 	}
@@ -23,20 +23,32 @@ public class PaintActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		this.getMenuInflater().inflate(R.menu.paint, menu);
-		return true;
+
+		return super.onCreateOptionsMenu(menu);
 	}
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
+	public boolean showPopup(View v) {
+		PopupMenu popup = new PopupMenu(this, v);
 		MenuInflater inflater = this.getMenuInflater();
 
 		switch (v.getId()) {
 		case R.id.action_file:
-			inflater.inflate(R.menu.file_context_menu, menu);
+			inflater.inflate(R.menu.file_popup_menu, popup.getMenu());
 			break;
 		}
+		
+		popup.show();
+
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_file)
+			showPopup(findViewById(id));
+
+		return true;
 	}
 
 }
